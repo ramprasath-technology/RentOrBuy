b6943b7a8a4b351eea1570055974440085bb6a2a
Ramprasath Babu <ramprasath.technology@gmail.com>
2024-01-21 16:47:08 -0600

    Add class library to add commonly used extension methods

---
+++ CommonExtensions/MathExtensions/DecimalExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonExtensions.MathExtensions
+{
+    public static class DecimalExtensions
+    {
+        public static decimal RoundToTwoDecimalPlaces(this decimal number)
+        {
+            return Math.Round(number, 2, MidpointRounding.AwayFromZero);
+        }
+    }
+}
