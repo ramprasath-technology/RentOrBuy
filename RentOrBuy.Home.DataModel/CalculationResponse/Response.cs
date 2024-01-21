e56135e7c4210ea4834422e4ce039ed665fecda2
Ramprasath Babu <ramprasath.technology@gmail.com>
2024-01-21 16:41:29 -0600

    Add models for user input and response

---
+++ RentOrBuy.Home.DataModel/CalculationResponse/Response.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentOrBuy.Home.DataModel.CalculationResponse
+{
+    public class Response
+    {
+        public Action Action { get; set; }
+        public decimal TotalSaving { get; set; }
+        public string ResponseMessage { get; set; } = string.Empty;
+    }
+
+    public enum Action
+    {
+        [Description("Own")]
+        Own = 0,
+        [Description("Rent")]
+        Rent
+    }
+}
---
+++ RentOrBuy.Home.DataModel/UserInput.cs
@@ -0,0 +1,18 @@
+﻿using RentOrBuy.Home.DataModel.EconomicCost;
+using RentOrBuy.Home.DataModel.OwnershipCost;
+using RentOrBuy.Home.DataModel.RentCost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentOrBuy.Home.DataModel
+{
+    public record UserInput
+    {
+        public OwnershipCostFactors OwnershipCosts { get; set; } = new OwnershipCostFactors();
+        public RentCostFactors RentCosts { get; set; } = new RentCostFactors();
+        public EconomicCostFactors EconomicCosts { get; set; } = new EconomicCostFactors();
+    }
+}
