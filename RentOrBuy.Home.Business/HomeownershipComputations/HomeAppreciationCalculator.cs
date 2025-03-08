085e7c47b11fe27c940358fd8ccac579d928e911
Ramprasath Babu <ramprasath.technology@gmail.com>
2025-03-08 16:01:25 -0600

    Fix defect with percentage calculation and add computation for home insurance

---
+++ RentOrBuy.Home.Business/HomeownershipComputations/HomeAppreciationCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentOrBuy.Home.Business.HomeownershipComputations
+{
+    internal class HomeAppreciationCalculator
+    {
+    }
+}
