6c92fe72cee322959dcae99401cff599b8a15906
Ramprasath Babu <ramprasath.technology@gmail.com>
2025-03-09 16:22:19 -0500

    Fix defect - rename MonthlyRent property to YearlyRent

--- RentOrBuy.Home.DataModel/RentCost/RentCostFactors.cs
+++ RentOrBuy.Home.DataModel/RentCost/RentCostFactors.cs
@@ -8,7 +8,7 @@ namespace RentOrBuy.Home.DataModel.RentCost
 {
     public record RentCostFactors
     {
-        public decimal MonthlyRent { get; set; }
+        public decimal YearlyRent { get; set; }
         public decimal AnnualRentGrowthRate { get; set; }
         public decimal RentersInsuranceInPercentage { get; set; }
     }
